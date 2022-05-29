293ed5f0417b67ef6a8696ed695ed46903ff7f9b
Neil <neiluv@icloud.com>
2022-05-29 00:49:57 -0700

    Created return visitor to aid in translations

---
+++ src/main/java/IR/Visitors/IRReturnVisitor.java
@@ -0,0 +1,37 @@
+package IR.Visitors;
+
+import IR.IRNodes.*;
+
+import java.util.List;
+
+public interface IRReturnVisitor<R> {
+    R visitAllInstructions(List<IRNode> instructions);
+
+    R visit(ForLoop forLoop);
+
+    R visit(Add add);
+
+    R visit(Subtract subtract);
+
+    R visit(Multiply multiply);
+
+    R visit(Divide divide);
+
+    R visit(Equals equals);
+
+    R visit(LessThan lessThan);
+
+    R visit(IfZero ifZero);
+
+    R visit(LabelNode labelNode);
+
+    R visit(IfNotEquals ifNotEquals);
+
+    R visit(Goto aGoto);
+
+    R visit(Print print);
+
+    R visit(Load load);
+
+    R visit(Store store);
+}
