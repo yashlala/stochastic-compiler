88f7d8a28b974f09ebf108fefc8a10fab3e3a8f2
Neil Vaishampayan <neiluv@icloud.com>
2022-06-04 00:25:44 -0700

    added for loop functionality

---
+++ src/main/java/IR/Visitors/CompilerVisitor.java
@@ -0,0 +1,165 @@
+package IR.Visitors;
+
+import IR.IRNodes.*;
+import ISA.InstructionNodes.BinaryJz;
+import ISA.InstructionNodes.LoadLiteralIns;
+import ISA.Labels.Label;
+import IR.Variables.Variable;
+import ISA.InstructionNodes.BinaryAdd;
+import ISA.InstructionNodes.InstructionNode;
+import ISA.Literals.Literal;
+import ISA.Registers.BinaryRegister;
+import ISA.Registers.StochasticRegister;
+import com.google.common.collect.ImmutableSet;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class CompilerVisitor implements IRReturnVisitor<List<InstructionNode>> {
+    private final BinaryRegister zeroReg = new BinaryRegister("zero");
+
+    @NonNull
+    private final ImmutableSet<Variable> stochasticVariables;
+
+    //two key sets are mutually exclusive
+    private final Map<Variable, BinaryRegister> binaryRegisterMap = new HashMap<>();
+    private final Map<Variable, StochasticRegister> stochasticRegisterMap = new HashMap<>();
+    private int counter = 0;
+
+    private String getNextRegisterName() {
+        return "t" + counter++;
+    }
+
+    private String getNextLabelName() {
+        return "L" + counter++;
+    }
+
+    private BinaryRegister getBinaryRegisterForVar(Variable variable) {
+        if (binaryRegisterMap.containsKey(variable)) {
+            return binaryRegisterMap.get(variable);
+        } else {
+            BinaryRegister nextReg = new BinaryRegister(getNextRegisterName());
+            binaryRegisterMap.put(variable, nextReg);
+            return nextReg;
+        }
+    }
+
+    @Override
+    public List<InstructionNode> visitAllInstructions(List<IRNode> instructions) {
+        List<InstructionNode> allIns = new LinkedList<>();
+        for (IRNode instruction : instructions) {
+            allIns.addAll(instruction.accept(this));
+        }
+        return allIns;
+    }
+
+    @Override
+    public List<InstructionNode> visit(ForLoop forLoop) {
+        List<InstructionNode> forLoopIns = new LinkedList<>();
+        //invariant: loop condition must be binary, new, and unused in loop body
+        BinaryRegister loopVar = getBinaryRegisterForVar(forLoop.getLoopIterVar());
+        BinaryRegister loopStart = getBinaryRegisterForVar(forLoop.getLoopRangeStart());
+        //init reg for one
+        BinaryRegister one = new BinaryRegister(getNextRegisterName());
+        forLoopIns.add(new LoadLiteralIns(one, new Literal(1)));
+        //init loop var
+        forLoopIns.add(new BinaryAdd(loopVar, loopStart, zeroReg));
+
+        Label loopStartLabel = new Label(getNextLabelName());
+        Label loopEndLabel = new Label(getNextLabelName());
+        forLoopIns.add(new ISA.InstructionNodes.LabelNode(loopStartLabel));
+
+        //less than check
+        BinaryRegister loopEnd = getBinaryRegisterForVar(forLoop.getLoopRangeEnd());
+        BinaryRegister tempCondCheck = new BinaryRegister(getNextRegisterName());
+        forLoopIns.add(new ISA.InstructionNodes.LessThan(tempCondCheck, loopVar, loopEnd));
+        //jump if not less than
+        forLoopIns.add(new BinaryJz(loopEndLabel, tempCondCheck));
+
+        //loop body
+        forLoop.getContents().forEach(irNode -> forLoopIns.addAll(irNode.accept(this)));
+
+        //increment loop var
+        forLoopIns.add(new BinaryAdd(loopVar, loopVar, one));
+        //jump to loop start
+        forLoopIns.add(new BinaryJz(loopStartLabel, zeroReg));
+        //end label
+        forLoopIns.add(new ISA.InstructionNodes.LabelNode(loopEndLabel));
+        return forLoopIns;
+    }
+
+    @Override
+    public List<InstructionNode> visit(Add add) {
+        return null;
+    }
+
+    @Override
+    public List<InstructionNode> visit(Subtract subtract) {
+        return null;
+    }
+
+    @Override
+    public List<InstructionNode> visit(Multiply multiply) {
+        return null;
+    }
+
+    @Override
+    public List<InstructionNode> visit(Divide divide) {
+        return null;
+    }
+
+    @Override
+    public List<InstructionNode> visit(Equals equals) {
+        return null;
+    }
+
+    @Override
+    public List<InstructionNode> visit(LessThan lessThan) {
+        return null;
+    }
+
+    @Override
+    public List<InstructionNode> visit(IfZero ifZero) {
+        return null;
+    }
+
+    @Override
+    public List<InstructionNode> visit(LabelNode labelNode) {
+        return null;
+    }
+
+    @Override
+    public List<InstructionNode> visit(IfNotEquals ifNotEquals) {
+        return null;
+    }
+
+    @Override
+    public List<InstructionNode> visit(Goto aGoto) {
+        return null;
+    }
+
+    @Override
+    public List<InstructionNode> visit(Print print) {
+        return null;
+    }
+
+    @Override
+    public List<InstructionNode> visit(Load load) {
+        return null;
+    }
+
+    @Override
+    public List<InstructionNode> visit(Store store) {
+        return null;
+    }
+
+    @Override
+    public List<InstructionNode> visit(SetLiteral setLiteral) {
+        return null;
+    }
+}
