dfd4e2ea74143f2fc8d60d8af10b09a7173c7370
Neil Vaishampayan <neiluv@icloud.com>
2022-06-06 16:17:40 -0700

    added Add compilation functionality, waiting on reponse from Yash for post add scaling

--- src/main/java/IR/Visitors/CompilerVisitor.java
+++ src/main/java/IR/Visitors/CompilerVisitor.java
@@ -1,11 +1,11 @@
 package IR.Visitors;
 
-import IR.IRNodes.*;
 import IR.IRNodes.LabelNode;
 import IR.IRNodes.LessThan;
+import IR.IRNodes.*;
+import IR.Variables.Variable;
 import ISA.InstructionNodes.*;
 import ISA.Labels.Label;
-import IR.Variables.Variable;
 import ISA.Literals.Literal;
 import ISA.Registers.BinaryRegister;
 import ISA.Registers.Register;
@@ -23,12 +23,16 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class CompilerVisitor implements IRReturnVisitor<List<InstructionNode>> {
     private final BinaryRegister zeroReg = new BinaryRegister("zero");
-    private final Variable additionScaleFactor = new Variable("$addScale");
+    private final Variable addS = new Variable("$addScale");
+    private final Variable scaleFactorVariable = new Variable("$scaleFactor");
+    private boolean scaleFactorInitialized = false;
+    private boolean addSInitialized = false;
 
     @NonNull
     private final ImmutableSet<Variable> stochasticVariables;
     private final int bitstreamWidth;
     private final RegisterPolarity polarity;
+    private final int scaleFactor;
 
     //two key sets are mutually exclusive
     private final Map<Variable, BinaryRegister> binaryRegisterMap = new HashMap<>();
@@ -125,8 +129,10 @@ public class CompilerVisitor implements IRReturnVisitor<List<InstructionNode>> {
             StochasticRegister dest = getStochasticRegisterForVar(add.getDest());
             StochasticRegister src1 = getStochasticRegisterForVar(add.getSrc1());
             StochasticRegister src2 = getStochasticRegisterForVar(add.getSrc2());
-            //TODO: Ask YASH about scale - always 0.5
-            ISAadd.add(new StochasticAdd(dest, src1, src2));
+            //TODO: rescale by 2
+            BinaryRegister addSReg = getBinaryRegisterForVar(addS);
+            initAddSIfNeeded(ISAadd);
+            ISAadd.add(new StochasticAdd(dest, src1, src2, addSReg));
         } else if (stocharg1 == stocharg2) {
             //if srcs agree on type, perform relevant operation then cast to dest type
             Register tempDest;
@@ -134,17 +140,107 @@ public class CompilerVisitor implements IRReturnVisitor<List<InstructionNode>> {
                 StochasticRegister src1 = getStochasticRegisterForVar(add.getSrc1());
                 StochasticRegister src2 = getStochasticRegisterForVar(add.getSrc1());
                 tempDest = new StochasticRegister(getNextRegisterName(), polarity, bitstreamWidth);
-                //TODO: Ask YASH about scale
-                ISAadd.add(new StochasticAdd((StochasticRegister) tempDest, src1, src2));
+                //TODO: rescale by 2
+                BinaryRegister addSReg = getBinaryRegisterForVar(addS);
+                initAddSIfNeeded(ISAadd);
+                ISAadd.add(new StochasticAdd((StochasticRegister) tempDest, src1, src2, addSReg));
             } else {
                 BinaryRegister src1 = getBinaryRegisterForVar(add.getSrc1());
                 BinaryRegister src2 = getBinaryRegisterForVar(add.getSrc1());
                 tempDest = new BinaryRegister(getNextRegisterName());
                 ISAadd.add(new BinaryAdd((BinaryRegister) tempDest, src1, src2));
             }
-
+            //invariant, dest reg is opposite of tempDest
+            if (stochDest) {
+                StochasticRegister dest = getStochasticRegisterForVar(add.getDest());
+                ISAadd.addAll(convertBinarytoStochastic((BinaryRegister) tempDest, dest));
+            } else {
+                BinaryRegister dest = getBinaryRegisterForVar(add.getDest());
+                ISAadd.addAll(convertStochasticToBinary((StochasticRegister) tempDest, dest));
+            }
+        } else {
+            //src disagree on type, so convert to dest type
+            if (stochDest) {
+                StochasticRegister dest = getStochasticRegisterForVar(add.getDest());
+                StochasticRegister src1;
+                StochasticRegister src2;
+                if (stocharg1) {
+                    src1 = getStochasticRegisterForVar(add.getSrc1());
+                    //guaranteed src2 is binary
+                    src2 = new StochasticRegister(getNextRegisterName(), polarity, bitstreamWidth);
+                    BinaryRegister tempSrc2 = getBinaryRegisterForVar(add.getSrc2());
+                    ISAadd.addAll(convertBinarytoStochastic(tempSrc2, src2));
+                } else {
+                    src2 = getStochasticRegisterForVar(add.getSrc2());
+                    //guaranteed src1 is binary
+                    src1 = new StochasticRegister(getNextRegisterName(), polarity, bitstreamWidth);
+                    BinaryRegister tempSrc1 = getBinaryRegisterForVar(add.getSrc1());
+                    ISAadd.addAll(convertBinarytoStochastic(tempSrc1, src1));
+                }
+                //TODO: rescale by 2
+                BinaryRegister addSReg = getBinaryRegisterForVar(addS);
+                initAddSIfNeeded(ISAadd);
+                ISAadd.add(new StochasticAdd(dest, src1, src2, addSReg));
+            } else {
+                BinaryRegister dest = getBinaryRegisterForVar(add.getDest());
+                BinaryRegister src1;
+                BinaryRegister src2;
+                if (!stocharg1) {
+                    src1 = getBinaryRegisterForVar(add.getSrc1());
+                    //guaranteed src2 is stoch
+                    src2 = new BinaryRegister(getNextRegisterName());
+                    StochasticRegister tempSrc2 = getStochasticRegisterForVar(add.getSrc2());
+                    ISAadd.addAll(convertStochasticToBinary(tempSrc2, src2));
+                } else {
+                    src2 = getBinaryRegisterForVar(add.getSrc2());
+                    //guaranteed src1 is stoch
+                    src1 = new BinaryRegister(getNextRegisterName());
+                    StochasticRegister tempSrc1 = getStochasticRegisterForVar(add.getSrc1());
+                    ISAadd.addAll(convertStochasticToBinary(tempSrc1, src1));
+                }
+                ISAadd.add(new BinaryAdd(dest, src1, src2));
+            }
         }
         return ISAadd;
+    }
+
+    private void initAddSIfNeeded(List<InstructionNode> ins) {
+        if (!addSInitialized) {
+            //creates mapping for scaleFactorReg
+            BinaryRegister scaleFactorReg = getBinaryRegisterForVar(addS);
+            addSInitialized = true;
+            ins.add(new LoadLiteralIns(scaleFactorReg, new Literal(0.5)));
+        }
+    }
+
+    private void initScaleFactorIfNeeded(List<InstructionNode> ins) {
+        if (!scaleFactorInitialized) {
+            BinaryRegister scaleFactorReg = getBinaryRegisterForVar(scaleFactorVariable);
+            scaleFactorInitialized = true;
+            ins.add(new LoadLiteralIns(scaleFactorReg, new Literal(scaleFactor)));
+        }
+    }
+
+    private List<InstructionNode> convertBinarytoStochastic(BinaryRegister src, StochasticRegister converted) {
+        List<InstructionNode> conversionIns = new LinkedList<>();
+        BinaryRegister scaleFactorReg = getBinaryRegisterForVar(scaleFactorVariable);
+        initScaleFactorIfNeeded(conversionIns);
+        //scale down the binary value into -1,1 range
+        BinaryRegister scaledDownBin = new BinaryRegister(getNextRegisterName());
+        conversionIns.add(new BinaryDiv(scaledDownBin, src, scaleFactorReg));
+        //convert to stochastic register
+        conversionIns.add(new BinaryToStochasticIns(converted, src));
+        return conversionIns;
+    }
+
+    private List<InstructionNode> convertStochasticToBinary(StochasticRegister src, BinaryRegister converted) {
+        List<InstructionNode> conversionIns = new LinkedList<>();
+        BinaryRegister tempScaledDownBin = new BinaryRegister(getNextRegisterName());
+        conversionIns.add(new StochasticToBinaryIns(tempScaledDownBin, src));
+        //upscales back to binary values range -f, f
+        BinaryRegister scaleFactorReg = getBinaryRegisterForVar(scaleFactorVariable);
+        conversionIns.add(new BinaryMul(converted, tempScaledDownBin, scaleFactorReg));
+        return conversionIns;
     }
 
     @Override
