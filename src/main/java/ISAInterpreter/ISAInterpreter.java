678f1ab9ea7bf3cbf5fe907dd958dc91d25fc660
Yash Lala <yashlala@gmail.com>
2022-06-06 18:17:35 -0700

    Complete final ISAInterpreter toplevel class interface.

--- src/main/java/ISAInterpreter/ISAInterpreter.java
+++ src/main/java/ISAInterpreter/ISAInterpreter.java
@@ -11,13 +11,18 @@ import java.util.Map;
 @Getter
 @RequiredArgsConstructor
 public class ISAInterpreter {
-    // Should this return a list of things to print? That would avoid side effects from printing directly.
     public static void executeProgram(List<InstructionNode> instructions) {
+        for (String s : getProgramOutput(instructions)) {
+            System.out.println(s);
+        }
+    }
+
+    public static List<String> getProgramOutput(List<InstructionNode> instructions) {
         // Scan the program for all labels.
         LabelScanningVisitor labelScanningVisitor = new LabelScanningVisitor();
         Map<Label, Integer> labelIndex = labelScanningVisitor.buildLabelIndex(instructions);
 
         ExecutionVisitor executionVisitor = new ExecutionVisitor();
-        executionVisitor.executeProgram(instructions, labelIndex);
+        return executionVisitor.executeProgram(instructions, labelIndex);
     }
 }
