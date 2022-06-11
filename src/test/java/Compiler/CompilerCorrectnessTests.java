97a1ebe5d964e4742f5b73bb6042951cee47f551
Neil <neiluv@icloud.com>
2022-06-10 17:54:23 -0700

    all tests finished, merge pending on decision of how to address less than bug

--- src/test/java/Compiler/CompilerCorrectnessTests.java
+++ src/test/java/Compiler/CompilerCorrectnessTests.java
@@ -60,6 +60,7 @@ public class CompilerCorrectnessTests {
         }
     }
 
+    //highly sensitive to stoch conv
     @Test
     void equalsTest() {
         List<IRNode> addProgram = new LinkedList<>();
@@ -67,6 +68,26 @@ public class CompilerCorrectnessTests {
         Variable b = new Variable("b");
         Variable c = new Variable("c");
         addProgram.add(new SetLiteral(a, new Literal(2.25)));
+        addProgram.add(new SetLiteral(b, new Literal(2.25)));
+        addProgram.add(new Equals(c, a, b));
+        addProgram.add(new Print(c));
+        List<List<Double>> res = getAllStochPerms(addProgram, a, b, c);
+        for (int i = 1; i < 8; i++) {
+            if (i/ 4 == 1) {
+                assertEquals(res.get(0).get(0), F * res.get(i).get(0), delta);
+            } else {
+                assertEquals(res.get(0).get(0), res.get(i).get(0), delta);
+            }
+        }
+    }
+
+    @Test
+    void notEqualsTest() {
+        List<IRNode> addProgram = new LinkedList<>();
+        Variable a = new Variable("a");
+        Variable b = new Variable("b");
+        Variable c = new Variable("c");
+        addProgram.add(new SetLiteral(a, new Literal(2.5)));
         addProgram.add(new SetLiteral(b, new Literal(2.25)));
         addProgram.add(new Equals(c, a, b));
         addProgram.add(new Print(c));
@@ -128,6 +149,136 @@ public class CompilerCorrectnessTests {
         List<InstructionNode> isa = vis.visitAllInstructions(program);
         List<Double> res = toDoubles(ISAInterpreter.getProgramOutput(isa, 0));
         assertEquals(0, res.size());
+    }
+
+    //highly sensitive (like equality)
+    @Test
+    void ifneTest() {
+        List<IRNode> program = new LinkedList<>();
+        Variable a = new Variable("a");
+        Variable b = new Variable("b");
+        Variable c = new Variable("c");
+        Variable d = new Variable("d");
+        Variable e = new Variable("e");
+        Label jump1 = new Label("jump1");
+        Label end1 = new Label("end1");
+        Label jump2 = new Label("jump2");
+        Label end2 = new Label("end2");
+        program.add(new SetLiteral(a, new Literal(2.5)));
+        program.add(new SetLiteral(b, new Literal(2.25)));
+        program.add(new SetLiteral(c, new Literal(2.25)));
+        program.add(new SetLiteral(d, new Literal(37)));
+        program.add(new SetLiteral(e, new Literal(17)));
+        program.add(new IfNotEquals(a,b,jump1));
+        program.add(new Print(d));
+        program.add(new Goto(end1));
+        program.add(new LabelNode(jump1));
+        program.add(new Print(e));
+        program.add(new LabelNode(end1));
+        program.add(new IfNotEquals(b,c,jump2));
+        program.add(new Print(e));
+        program.add(new Goto(end2));
+        program.add(new LabelNode(jump2));
+        program.add(new Print(d));
+        program.add(new LabelNode(end2));
+        List<List<Double>> res = getAllStochPerms(program, a, b, c);
+        for (int i = 0; i < 8; i++) {
+            System.out.println(i);
+            List<Double> testResults = res.get(i);
+            assertEquals(2, testResults.size());
+            assertEquals(17, testResults.get(0));
+            assertEquals(17, testResults.get(1));
+        }
+    }
+
+    @Test
+    void ifZeroTest() {
+        List<IRNode> program = new LinkedList<>();
+        Variable a = new Variable("a");
+        Variable b = new Variable("b");
+        Variable c = new Variable("c");
+        Variable d = new Variable("d");
+        Variable e = new Variable("e");
+        Label jump1 = new Label("jump1");
+        Label end1 = new Label("end1");
+        Label jump2 = new Label("jump2");
+        Label end2 = new Label("end2");
+        program.add(new SetLiteral(a, new Literal(0)));
+        program.add(new SetLiteral(b, new Literal(0.1)));
+        program.add(new SetLiteral(d, new Literal(37)));
+        program.add(new SetLiteral(e, new Literal(17)));
+        program.add(new IfZero(a,jump1));
+        program.add(new Print(d));
+        program.add(new Goto(end1));
+        program.add(new LabelNode(jump1));
+        program.add(new Print(e));
+        program.add(new LabelNode(end1));
+        program.add(new IfZero(b,jump2));
+        program.add(new Print(e));
+        program.add(new Goto(end2));
+        program.add(new LabelNode(jump2));
+        program.add(new Print(d));
+        program.add(new LabelNode(end2));
+        List<List<Double>> res = getAllStochPerms(program, a, b, c);
+        for (int i = 0; i < 8; i++) {
+            System.out.println(i);
+            List<Double> testResults = res.get(i);
+            assertEquals(2, testResults.size());
+            assertEquals(17, testResults.get(0));
+            assertEquals(17, testResults.get(1));
+        }
+    }
+
+    //FAILS DUE TO INTERPRETER BUG
+    @Test
+    void lessThanTest() {
+        List<IRNode> addProgram = new LinkedList<>();
+        Variable a = new Variable("a");
+        Variable b = new Variable("b");
+        Variable c = new Variable("c");
+        addProgram.add(new SetLiteral(a, new Literal(2.5)));
+        addProgram.add(new SetLiteral(b, new Literal(3.25)));
+        addProgram.add(new LessThan(c, a, b));
+        addProgram.add(new Print(c));
+        List<List<Double>> res = getAllStochPerms(addProgram, a, b, c);
+        for (int i = 1; i < 8; i++) {
+            System.out.println(i);
+            if (i/ 4 == 1) {
+                assertEquals(res.get(0).get(0), F * res.get(i).get(0), delta);
+            } else {
+                assertEquals(res.get(0).get(0), res.get(i).get(0), delta);
+            }
+        }
+    }
+
+    @Test
+    void loadStoreTest() {
+        List<IRNode> program = new LinkedList<>();
+        Variable a = new Variable("a");
+        Variable b = new Variable("b");
+        Variable c = new Variable("c");
+        Variable d = new Variable("d");
+        program.add(new SetLiteral(a, new Literal(3.49)));
+        program.add(new SetLiteral(b, new Literal(4.49)));
+        program.add(new SetLiteral(c, new Literal(7)));
+        program.add(new SetLiteral(d, new Literal(9)));
+        program.add(new Store(a, c));
+        program.add(new Store(b, d));
+        program.add(new Load(c, a));
+        program.add(new Load(d, b));
+        program.add(new Print(c));
+        program.add(new Print(d));
+        List<List<Double>> res = getAllStochPerms(program, a, b, c);
+        for (int i = 0; i < 8; i++) {
+            List<Double> testRes = res.get(i);
+            assertEquals(2, testRes.size());
+            if (i/ 4 == 1) {
+                assertEquals(7, F*testRes.get(0), delta);
+            } else {
+                assertEquals(7, testRes.get(0), delta);
+            }
+            assertEquals(9, testRes.get(1));
+        }
     }
 
     @Test
