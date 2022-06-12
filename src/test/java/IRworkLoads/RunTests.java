d9aa04d870b18f351544f6fcbb7e956249a001f1
salekh <salekh@cs.ucla.edu>
2022-06-12 12:26:18 -0700

    matrix matrix multiplication bug fixed

--- src/test/java/IRworkLoads/RunTests.java
+++ src/test/java/IRworkLoads/RunTests.java
@@ -14,34 +14,32 @@ import java.util.*;
 
 public
 class RunTests {
-    public void runDotProduct(int N, Variable v, int noiseCoefficient, int bitWidth,int scalingFactor ){
+    public void runDotProduct(int N, Variable v,  int bitWidth,int scalingFactor ,int noiseCoefficient){
         List< IRNode > instructions = new LinkedList<>();
         DotProduct dotProduct = new DotProduct();
         instructions.addAll( dotProduct.calculateDotProduct(N,v,0));
         instructions.add(new Print(v));
         Selector selector = new Selector();
         ImmutableSet<Variable> stochVars = ImmutableSet.copyOf(selector.collectAllRegisters(instructions));
-//        System.out.println("IR code");
-//        for(IRNode i: instructions){
-//            System.out.println(i);
-//        }
-//        instructions = selector.generateTest();
         List< InstructionNode > isaInstructions= this.generateISAcode(instructions,scalingFactor,bitWidth, stochVars);
-//        System.out.println("\n\n ISA instructions \n\n");
-//        for(InstructionNode i: isaInstructions){
-//            System.out.println(i);
-//        }
-
         this.executeISAcode(isaInstructions,noiseCoefficient);
     }
     public  void runMatVectMult(){
         List< IRNode > instructions = new LinkedList<>();
         MatVectMult matVectMult = new MatVectMult();
     }
-    public void runMatMatMult(Integer N, Integer M){
+    public void runMatMatMult(Integer N, Integer M, List<Variable> output,int scalingFactor,int bitWidth, int noiseCoefficient){
         List< IRNode > instructions = new LinkedList<>();
         MatMult matMult = new MatMult();
-//        instructions = matMult.calculateMatMult(N,M,new Variable("output"));
+        instructions = matMult.calculateMatMult(N,M,output);
+        System.out.println("\n\n\nGenerated IR\n\n\n");
+        for(IRNode i: instructions){
+            System.out.println(i);
+        }
+        Selector selector = new Selector();
+        ImmutableSet<Variable> stochVars = ImmutableSet.copyOf(selector.collectAllRegisters(instructions));
+        List< InstructionNode > isaInstructions= this.generateISAcode(instructions,scalingFactor,bitWidth, stochVars);
+        this.executeISAcode(isaInstructions,noiseCoefficient);
     }
     public void runConv(){
         List< IRNode > instructions = new LinkedList<>();
@@ -67,12 +65,25 @@ class RunTests {
     public static void main(String[] args){
         List< IRNode > instructions = new LinkedList<>();
         RunTests runTests = new RunTests();
+        //Specify the test parameters
         int noiseCoefficient = 10;
-        int scalingFactor = 5;
+        int scalingFactor = 25000;
         int bitWidth = 64;
-        int N = 2;
-        Variable output = new Variable("output");
-        runTests.runDotProduct(N,output,noiseCoefficient,bitWidth,scalingFactor);
+        int N = 20;
+        int M = 30;
+        //Uncomment to run the test for the dotProduct
+        //Specify the register name for the final result to be stored
+//        Variable output = new Variable("output");
+//        runTests.runDotProduct(N,output,bitWidth,scalingFactor,noiseCoefficient);
+
+
+        //Uncomment to run the test for matrix marix multiply
+        //Specify the register names for the final results to be stored
+        List<Variable> output = new LinkedList <>();
+        for(int i=0; i<N;i++){
+            output.add(new Variable("output_"+i));
+        }
+        runTests.runMatMatMult(N,M,output,scalingFactor,bitWidth,noiseCoefficient);
 
         runTests.runConv();
     }
